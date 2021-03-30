db2b395b67159cdd06ea54f82828de16e348d9e9
GillesVanDeVyver <gillesvandevyver@hotmail.be>
2021-03-30 11:04:01 +0200

    modern sw working

--- PersistentHashedIndex.java
+++ PersistentHashedIndex.java
@@ -9,7 +9,6 @@ package ir;
 
 import java.io.*;
 import java.util.*;
-import com.google.common.primitives.Longs;
 import ir.PersistentHashedIndex.Entry;
 import java.nio.ByteBuffer;
 import java.nio.charset.*;
